16feeb60309b09acc1ed6144b0ba998e741e208b
pj <pjsaez96@gmail.com>
2022-06-15 18:36:53 +0200

    final structure

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-// var Route = artifacts.require("Route");
-// var SensorFactory = artifacts.require("SensorFactory");
 var RouteFactory = artifacts.require("RouteFactory");
-//var address = "0x5aD56bEEE5980a974AE121257b61A16E882407D9";
+
 module.exports = deployer => {
     deployer.deploy(RouteFactory);
-        
-    //deployer.deploy(SensorFactory);
 };
